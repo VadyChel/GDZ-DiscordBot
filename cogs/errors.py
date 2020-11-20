967ab12bc6d392fdc27ae6ecc431776e520bfa08
VadyChel <zyzel19@gmail.com>
2020-11-20 17:30:44 +0200

    Add wraper for errors and add base commands

--- cogs/errors.py
+++ cogs/errors.py
@@ -6,6 +6,18 @@ class Errors(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx, error):
+		if isinstance(error, commands.errors.CommandNotFound):
+			pass
+		elif isinstance(error, commands.errors.MissingRequiredArgument):
+			await ctx.send(
+				embed=discord.Embed(
+					title="Ошибка!",
+					description="Укажите номер задания!",
+					colour=discord.Color.red()
+				)
+			)
 
 def setup(client):
 	client.add_cog(Errors(client))
--- cogs/gdz.py
+++ cogs/gdz.py
@@ -6,6 +6,27 @@ class GDZ(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
+	@commands.command(
+		aliases=["алгебра"],
+		description="Отправляет гдз по номеру из алгебры"
+	)
+	async def algebra(self, ctx, number):
+		pass
+
+	@commands.command(
+		aliases=["геометрия", "геометрія"],
+		description="Отправляет гдз по номеру из алгебры"
+
+	)
+	async def geometry(self, ctx, number):
+		pass
+
+	@commands.command(
+		aliases=["укр-мова", "укр-язык"],
+		description="Отправляет гдз по номеру из алгебры"
+	)
+	async def ukr_language(self, ctx, number):
+		pass
 
 def setup(client):
 	client.add_cog(GDZ(client))
